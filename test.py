e8b88d02922e508e6cbac7920a74589f89da54c5
John Cruz <john.cruz6725@gmail.com>
2019-11-21 16:24:27 -0800

    more stuff

--- test.py
+++ test.py
@@ -1,5 +1,32 @@
 import numpy as np
-edgelist = np.loadtxt('data/G1.txt', dtype = int )
+import os
+
+from functions.reading_writing_function import get_graph
+def star ():
+    print ('*******')
+G = get_graph('G1')
+
+os.system("cls")
 print ('*****')
-print (edgelist[0])
+print (f'V(G) = {G[0]}')
+print (f'V(E) = {G[1]}')
+
 print ('*****')
+
+
+#tree
+
+
+T = ([3, 1],[(1, 3)])
+star()
+
+def inceident_edgees (G,T):
+    edges = []
+    for e in G[1]:
+        for v in T[0]:
+            if v in e and e not in T[1]:
+                edges.append(e)
+                #print (e)
+    return edges
+
+print (inceident_edgees(G,T))
