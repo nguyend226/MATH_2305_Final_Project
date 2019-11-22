075fdde367ac704e7e79844d421fc4f6186b91f3
John Cruz <john.cruz6725@gmail.com>
2019-11-21 16:27:11 -0800

    moafm

--- test.py
+++ test.py
@@ -23,13 +23,4 @@ print ('*****')
 T = ([3, 1],[(1, 3)])
 star()
 
-def inceident_edgees (G,T):
-    edges = []
-    for e in G[1]:
-        for v in T[0]:
-            if v in e and e not in T[1]:
-                edges.append(e)
-                #print (e)
-    return edges
-
 print (inceident_edgees(G,T))
