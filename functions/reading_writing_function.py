6398c85e3c07e9049d66eea4519f664bcd320340
John Cruz <john.cruz6725@gmail.com>
2019-11-21 16:29:57 -0800

    save point, pro moving too fast

--- functions/reading_writing_function.py
+++ functions/reading_writing_function.py
@@ -5,14 +5,14 @@ def get_graph(textfile):
     edgelist = np.loadtxt(f'data/{textfile}.txt', dtype = int )
 
     G = ([],[])
+
     for x in edgelist:
+
         if x[0] not in G[0]:
             G[0].append(x[0])
 
         if x[1] not in G[0]:
             G[0].append(x[1])
-
-
 
         G[1].append((x[0],x[1]))
 
--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 import numpy as np
 import os
-
 from functions.reading_writing_function import get_graph
 from functions.graph_operations import inceident_edgees
 
 
 def star ():
     print ('*******')
+
+
 G = get_graph('G1')
 
 os.system("cls")
 print ('*****')
 print (f'V(G) = {G[0]}')
 print (f'V(E) = {G[1]}')
-
 print ('*****')
 
 
