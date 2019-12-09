f902db52e653b5937e5f581830621b2999473ad2
John Cruz <john.cruz6725@gmail.com>
2019-12-09 11:34:00 -0800

    making it pretty as well as adding comments

--- algorithms/prims_algorithms.py
+++ algorithms/prims_algorithms.py
@@ -1,17 +1,18 @@
-from functions.graph_operations import min_cost_incident_edge, inceident_edgees, initialize_tree
+from functions.graph_operations import min_cost_incident_edge, inceident_edgees, initialize_tree, cost
 
 
 def Prims(Graph, Starting_point):
+    '''Returns the minimum sapning tree as well and the total cost of that tree. '''
 
     Tree = initialize_tree(Starting_point)
     a = inceident_edgees(Graph, Tree)
     path_to_take = min_cost_incident_edge(Graph, Tree)
  
 
-    while (len(Tree[0]) != len(Graph[0])):
-        avalible_paths = inceident_edgees(Graph, Tree)
+    while (len(Tree[0]) != len(Graph[0])):                          #while the length of the tree's vertices is not the same lenght of the graph's vertices than do the operation 
+        avalible_paths = inceident_edgees(Graph, Tree)          
     
-        for path in avalible_paths:
+        for path in avalible_paths:                                 #Checks all avalible plaths
     
              if ( path == min_cost_incident_edge(Graph, Tree)):
                 Tree[1].append(path)
@@ -23,5 +24,11 @@ def Prims(Graph, Starting_point):
                         if vertice not in Tree[0]:
                             Tree[0].append(vertice)
       
-    return Tree
+    total_cost_of_tree = 0
+
+    for e in range(0, len( Tree) + 1 ):
+        total_cost_of_tree = total_cost_of_tree + cost(Graph, Tree[1][e] )
+
+
+    return Tree, total_cost_of_tree
 
--- main.py
+++ main.py
@@ -7,26 +7,25 @@ from functions.graph_operations import *
 os.system("cls")
 
 
-File = input(' get file')
+File = input(' Input name of filr that cotians the graph. :')
 
 
 
 
-Graph = get_graph("G1")
+Graph = get_graph("G1")# Add File here
 
-starting_point = int( input(f'{Graph[0]} /n pick a vertex')) 
-
-
-minimum_spanning_tree = Prims(Graph, starting_point)
+starting_point = int( input(f' {Graph[0]} \n Pick a vertex. :')) 
 
 
-#def total_cost_of_tree (G, ):
-#total_cost_of_tree = 0
+minimum_spanning_tree, total_cost_of_tree = Prims(Graph, starting_point)
 
 
-for edge in range(0, len( minimum_spanning_tree) + 1 ):
-    total_cost_of_tree = total_cost_of_tree + cost(Graph, minimum_spanning_tree[1][edge] )
 
-#print(minimum_spanning_tree[1][0])
 
-print (minimum_spanning_tree, total_cost_of_tree)
+
+
+
+print (f'''\n The minimun spanining tree {minimum_spanning_tree}
+ and the total cost of the tree {total_cost_of_tree} ''')
+
+
