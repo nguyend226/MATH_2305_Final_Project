fe3ea3e4607c838591cce9a1c23f36fad1f19792
Dat Nguyen <57651102+nguyend226@users.noreply.github.com>
2019-12-10 15:56:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 from algorithms.prims_algorithms import Prims
-import numpy as np
-import os
 from functions.reading_writing_function import get_graph
 from functions.graph_operations import *
+import os
 
 os.system("cls") # used to clear the console on windows machines
 
-
-File = input(' Input name of filr that cotians the graph. :')
-
-
-
+File = str(input(''' Input name of file that contians the graph, or use (G1, G2, G3, G4, or G5). 
+ DO NOT add file extension. : '''))
 
 Graph = get_graph(File) 
 
 starting_point = int( input(f' {Graph[0]} \n Pick a vertex. :')) 
 
 minimum_spanning_tree = Prims(Graph, starting_point)
+
 cost_of_tree = total_cost_of_tree(Graph, minimum_spanning_tree)
 
 vertices, edges = minimum_spanning_tree
 
 
-
 print (f'''\n The minimun spanining tree 
- Vetices: {vertices}
- Edges : {edges}
- and the total cost of the tree {total_cost_of_tree(Graph, minimum_spanning_tree)} ''')
-
+ Vetices: {vertices}, total: {len(vertices)}
+ Edges : {edges}, total: {len(edges)} 
+ Total cost of the tree: {total_cost_of_tree(Graph, minimum_spanning_tree)} ''')
