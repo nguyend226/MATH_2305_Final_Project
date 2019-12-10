a4c229060f83be0c35d97159ce53147eea0f4dba
Dat Nguyen <57651102+nguyend226@users.noreply.github.com>
2019-12-10 15:57:25 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,78 +1,36 @@
+from algorithms.prims_algorithms import Prims
 import numpy as np
 import os
 from functions.reading_writing_function import get_graph
 from functions.graph_operations import *
-os.system("cls")
+
+os.system("cls") # used to clear the console on windows machines
 
 
-def star (somestring=''):
-    
-    print (f'****{somestring}****')
-
-
-G = get_graph('G2')
-
-os.system("cls")
-
-star('raw graph')
-print (G)
-
-star('graph simplyfied')
-
-print (f'G(V) = {G[0]}')
-
-print (f'G(E) = {G[1]}')
-
-print (f'G(cost) = {G[1].values() }')
-
-star()
-
-
-
-#start point on tree ? 
-#T = ([3, 1, 4 ], [(1, 3), (1, 4)])
-#T = ([0,1,3,2], [(0,1), (1,3), (2,3)])
-
-T = ([5], [])
-
-star("all edges")
-for e in G[1]:
-    print (e)
-star()
-
-print (inceident_edgees(G,T))
-
-print(f'Tree={T}')
-
-star("All incident edges")
-for e in inceident_edgees(G,T):
-    print (f"Edge {e}")# with the cost {cost(G, e)}")
-star()
-
-
-star(f'{T[0]}')
-
-star("cost of 1,3")
-print (f"{cost(G, (1,3))}")
-star()
-
-star("min_cost_incedent_edge")
-print (f"{ min_cost_incident_edge (G, T)}")
-star()
-
-
-
-star('new section incenden testing')
-
-print (f'{inceident_edgees(G,T)}')
-#print(f"{g}")
-
-star()
+File = input(' Input name of file that cotians the graph. :')
 
 
 
 
+Graph = get_graph('G'+File) 
 
+starting_point = int( input(f' {Graph[0]} \n Pick a vertex. :')) 
 
+minimum_spanning_tree = Prims(Graph, starting_point)
+cost_of_tree = total_cost_of_tree(Graph, minimum_spanning_tree)
 
+vertices, edges = minimum_spanning_tree
 
+T =  ([0, 1, 4], [(0, 1), (1, 4)])
+
+print (f'''\n The minimun spanining tree 
+ Vetices: {vertices}, {len(vertices)}
+ Edges : {edges}, {len(edges)}
+ and the total cost of the tree {total_cost_of_tree(Graph, minimum_spanning_tree)} 
+ ********* prims output **********
+ {minimum_spanning_tree}
+******** incident_edges **********
+T =   {T}
+avalible paths to get last vertex. 
+{incident_edges(Graph, T)}
+ ''')
