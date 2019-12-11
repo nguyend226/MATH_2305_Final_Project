0b751f5040a572ae03f8744b432319e65c21018c
Dat Nguyen <57651102+nguyend226@users.noreply.github.com>
2019-12-10 23:58:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 os.system("cls") # used to clear the console on windows machines
 
-File = str(input(''' Input name of file that contians the graph, or use (G1, G2, G3, G4, or G5). 
+File = str(input(''' Input name of file that contians the graph, or use (G1, G2, G3, G4, G5 or FinalG). 
  DO NOT add file extension. : '''))
 
 Graph = get_graph(File) 
@@ -19,7 +19,7 @@ cost_of_tree = total_cost_of_tree(Graph, minimum_spanning_tree)
 vertices, edges = minimum_spanning_tree
 
 
-print (f'''\n The minimun spanning tree 
- Vertices: {vertices}, total: {len(vertices)}
+print (f'''\n The minimun spanining tree 
+ Vetices: {vertices}, total: {len(vertices)}
  Edges : {edges}, total: {len(edges)} 
  Total cost of the tree: {total_cost_of_tree(Graph, minimum_spanning_tree)} ''')
