ef5e018857f2618308736842cb756af37fa48208
Dat Nguyen <57651102+nguyend226@users.noreply.github.com>
2019-12-10 22:42:21 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,41 @@
 from algorithms.prims_algorithms import Prims
-import numpy as np
-import os
 from functions.reading_writing_function import get_graph
 from functions.graph_operations import *
+import os
 
 os.system("cls") # used to clear the console on windows machines
 
 
-File = input(' Input name of file that cotians the graph. :')
 
 
 
 
-Graph = get_graph('G'+File) 
 
-starting_point = int( input(f' {Graph[0]} \n Pick a vertex. :')) 
+
+T = ([0, 2, 1, 9, 4, 5, 6, 3, 11, 7, 8, 10], [(0, 2), (1, 2), (2, 9), (1, 4), (4, 5), (2, 6), (4, 3), (3, 11), (5, 7), (7, 8), (7, 10)])
+
+
+
+
+
+
+Graph = get_graph('FinalG') 
+
+
+#starting_point = int( input(f' {Graph[0]} \n Pick a vertex. :')) 
+starting_point = 3
+
+
+
 
 minimum_spanning_tree = Prims(Graph, starting_point)
+
 cost_of_tree = total_cost_of_tree(Graph, minimum_spanning_tree)
 
 vertices, edges = minimum_spanning_tree
 
-T =  ([0, 1, 4], [(0, 1), (1, 4)])
 
-print (f'''\n The minimun spanning tree 
- Vertices: {vertices}, {len(vertices)}
- Edges : {edges}, {len(edges)}
- and the total cost of the tree {total_cost_of_tree(Graph, minimum_spanning_tree)} 
- ********* prims output **********
- {minimum_spanning_tree}
-******** incident_edges **********
-T =   {T}
-avalible paths to get last vertex. 
-{incident_edges(Graph, T)}
- ''')
+print (f'''\n The minimun spanining tree 
+ Vetices: {vertices}, total: {len(vertices)}
+ Edges : {edges}, total: {len(edges)} 
+ Total cost of the tree: {total_cost_of_tree(Graph, minimum_spanning_tree)} ''')
